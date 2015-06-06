1b5263abebbc9a98f394216923665e6d22f8acb2
Pandada8 <pandada8@gmail.com>
2015-06-06 16:43:40 +0800

    go out

--- times/views.py
+++ times/views.py
@@ -1,13 +1,14 @@
 from times import app
-from flask import redirect, url_for, render_template
+from flask import redirect, url_for, render_template, send_from_directory, jsonify
 
-@app.route("/admin/")
+
+@app.route("/admin/api/login", methods=["GET", "POST"])
+def login_status():
+    return jsonify()
+
+@app.route("/admin/<path>")
 def admin_dashboard():
-    return redirect(url_for("admin_login"))
-
-@app.route("/admin/login")
-def admin_login():
-    return render_template("admin_login.html")
+    return send_from_directory("static/admin.html")
 
 @app.route("/")
 def show_index():
